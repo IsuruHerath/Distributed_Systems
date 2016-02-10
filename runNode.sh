f94f992833e29696dcb0bcbed93bf3dd8d75746d
isuru <isuruherath1@gmail.com>
2016-02-10 13:32:13 +0530

    removed runNode.sh

--- runNode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SERVER_HOST=127.0.0.1
-SERVER_PORT=8889
-NODE_PORT=$1
-USER_NAME=$2
-APPDIR=/home/isuru/Acacia/x10dt/workspace/Distributed_Systems
-cd $APPDIR/build/classes
-java test.Main $SERVER_HOST $SERVER_PORT $NODE_PORT $USER_NAME
